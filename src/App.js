41275687638a54def60c776c08ce2a7db07da5b1
delanobmarques <delanobmarques@gmail.com>
2022-08-11 15:17:35 -0300

    create and style settings page component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import TopBar from "./components/TopBar/topbar.component";
 import Home from "./pages/home/home.page";
+import Settings from "./pages/settings/settings.page";
 import Single from "./pages/single/single.page";
 import Write from "./pages/write/write.page";
 
@@ -8,7 +9,7 @@ function App() {
     <>
       <TopBar/>
       {/* <Home /> */}
-      <Write/>
+      <Settings />
     </>
   );
 }
---
+++ src/pages/settings/settings.page.jsx
@@ -0,0 +1,46 @@
+import SideBar from "../../components/SideBar/sidebar.component";
+import "./settings.styles.css";
+import profilePicture from "../../assets/cinthia-marques.png";
+
+const Settings = () => {
+  return (
+    <div className="settings">
+      <div className="settingsWrapper">
+        <div className="settingsTitle">
+          <span className="settingsTitleUpdate">Update Your Account</span>
+          <span className="settingsTitleDelete">Delete Account</span>
+        </div>
+        <form className="settingsForm">
+          <label>Profile Picture</label>
+          <div className="settingsPP">
+            <img
+              src={profilePicture}
+              alt="profile"
+            />
+            <label htmlFor="fileInput">
+              <i className="settingsPPIcon far fa-user-circle"></i>{" "}
+            </label>
+            <input
+              id="fileInput"
+              type="file"
+              style={{ display: "none" }}
+              className="settingsPPInput"
+            />
+          </div>
+          <label>Username</label>
+          <input type="text" placeholder="Cinthia" name="name" />
+          <label>Email</label>
+          <input type="email" placeholder="cinthiafrmarques@gmail.com" name="email" />
+          <label>Password</label>
+          <input type="password" placeholder="Password" name="password" />
+          <button className="settingsSubmitButton" type="submit">
+            Update
+          </button>
+        </form>
+      </div>
+      <SideBar/>
+    </div>
+  )
+}
+
+export default Settings
