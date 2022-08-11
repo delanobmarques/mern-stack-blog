380b423163746ec4a801c9100d820ecaecf18478
delanobmarques <delanobmarques@gmail.com>
2022-08-11 13:21:16 -0300

    create and style post and posts components, add z-index to top bar component

---
+++ src/components/Post/post.component.jsx
@@ -0,0 +1,30 @@
+import './post.styles.css';
+import postImage from "../../assets/casamento-09.jpeg";
+
+const Post = () => {
+  return (
+    <div className="post">
+        <img 
+            className="postImage" 
+            src={postImage} 
+            alt="post" 
+        />
+        <div className="postInfo">
+            <div className="postCats">
+                <div className="postCat">Cakes</div>
+                <div className="postCat">Decorations</div>
+            </div>
+            <span className="postTitle">
+                Lorem ipsum dolor sit amet consectetur adipisicing!
+            </span>
+            <hr />
+            <span className="postDate">1 hour ago</span>
+        </div>
+        <p className='postDesc'>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam numquam excepturi ratione ad aliquid iure deserunt, optio asperiores officiis explicabo ex velit aperiam facilis. Dolor quas hic cupiditate molestiae! Asperiores, veniam suscipit nisi, voluptates dolorem laboriosam deserunt debitis delectus unde est amet rerum perspiciatis non blanditiis, aperiam nobis aliquam consequuntur soluta vero ipsa reiciendis? Odio distinctio non harum id numquam!
+        </p>
+    </div>
+  )
+}
+
+export default Post
--- src/components/Posts/posts.component.jsx
+++ src/components/Posts/posts.component.jsx
@@ -1,8 +1,18 @@
+import Post from '../Post/post.component';
 import './posts.styles.css';
 
 const Posts = () => {
   return (
-    <div className='posts'>Posts</div>
+    <div className='posts'>
+      <Post/>
+      <Post/>
+      <Post/>
+      <Post/>
+      <Post/>
+      <Post/>
+      <Post/>
+      <Post/>
+    </div>
   )
 }
 
