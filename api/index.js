62e4e3cc65c6d56eea906f82ea402ea30c71b05a
delanobmarques <delanobmarques@gmail.com>
2022-08-11 22:27:25 -0300

    setup basic mongodb connection

--- api/index.js
+++ api/index.js
@@ -1,10 +1,19 @@
 // mongodb+srv://blog-app:<password>@cluster0.atgnbdl.mongodb.net/?retryWrites=true&w=majority
 
 import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 //initialize app
 const app = express();
 
+dotenv.config();
+
+//using mongoose to connect to database
+mongoose.connect(process.env.MONGO_URL)
+    .then(console.log("Connected to mongodb....."))
+    .catch((error) => console.log(`${error} did not connect`));
+
 app.listen("5000", () => {
-    console.log("server is running... ")
+    console.log("server is running... ");
 });
