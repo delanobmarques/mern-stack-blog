a24c318b77137034ac8f63e6cb29a73934d88715
delanobmarques <delanobmarques@gmail.com>
2022-10-05 19:18:50 -0300

    update auth route

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,8 +1,9 @@
 import express from "express";
 //setup router
-const router = express.Router();
+const router = express.Router(); // const router = require("express").Router();
 //import user model
 import User from "../models/User.js";
+//import bcrypt hashing library
 import bcrypt from 'bcryptjs';
 
 //REGISTER NEW USER ROUTE
@@ -27,29 +28,42 @@ router.post("/register", async(req, res) => {
 router.post("/login", async (req, res) => {
     try {
       const user = await User.findOne({ username: req.body.username });//finding unique user
-    //   !user && res.status(400).json("Wrong credentials!");//if there is no user 
-  
+      !user && res.status(400).json("Wrong credentials! (user)");//if there is no user 
+      console.log('........user............')
+      
       const validated = await bcrypt.compare(req.body.password, user.password);//compare typed password with encrypted one in db
-    //   !validated && res.status(400).json("Wrong credentials!");
-  
+      !validated && res.status(400).json("Wrong credentials! (password)");
+      console.log('........password...........')
+      
+      // res.status(200).json(user);
       const { password, ...others } = user._doc;//send everything but user password
-    //   res.status(200).json(others);
+      res.status(200).json(others);
 
-      if(user && validated) {
-        return res.status(200).json(others);
-      }else if(!user) {
-        return res.status(400).json("Wrong credentials!");//if there is no user 
-      }else if(!validated) {
-        return res.status(400).json("Wrong credentials!");
-      }
+    // ************ TO DO ************
+    // returns error if wrong user 
+    //******************************* */
+
+      // if(user && validated) {
+      //   console.log('user and password ok')
+      //   return res.status(200).json(others);
+      // }else{
+      //   return res.status(400).json("Wrong credentials!");//if there is no user 
+      // }
 
 
-
+      // }else if(!user) {
+      //   console.log('NO user!!! ')
+      //   return res.status(400).json("Wrong credentials!");//if there is no user 
+      // }else if(!validated) {
+      //   console.log('NO passwprd!!! ')
+      //   return res.status(400).json("Wrong credentials!");
+      // }
 
     } catch (err) {
-      res.status(500).json(err);
+      console.log(`Something went wrong...${err}`)
+      return res.status(500).json(err);
     }
   });
   
 export default router;
-
+// module.exports = router;
