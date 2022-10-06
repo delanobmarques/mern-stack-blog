f69684a7fb3a790c01f783fd8515eb556afd4a9e
delanobmarques <delanobmarques@gmail.com>
2022-10-06 10:34:46 -0300

    create state and useEffect hook to fetch data in Home component

--- client/src/pages/home/home.page.jsx
+++ client/src/pages/home/home.page.jsx
@@ -1,14 +1,29 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
 import Header from '../../components/Header/header.component';
 import Posts from '../../components/Posts/posts.component';
 import SideBar from '../../components/SideBar/sidebar.component';
 import './home.styles.css';
+import axios from 'axios';
+import { useLocation } from 'react-router-dom';
 
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+  const { search } = useLocation;
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const response = await axios.get("/posts");
+      setPosts(response.data);
+    };
+    getPosts();
+  }, [search]);
+
   return (
     <>
         <Header />
         <div className='home'>            
-            <Posts />
+            <Posts posts={posts} />
             <SideBar />
         </div>
     </>
